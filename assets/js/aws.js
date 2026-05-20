9fdeef679864087d5c11c0f575d824026e71d8a4
asiaworldshipping <asiaworldshipping.com@gmail.com>
2026-05-20 19:44:25 +0530

    Add files via upload

---
+++ assets/js/aws.js
@@ -0,0 +1,86 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function ($) {
+    $(document).ready(function () {
+        $(".animate").each(function() {
+            $(this).attr({
+                'data-aos': 'fade-up',
+                'data-aos-delay':'50'
+            });
+        });
+        $(".animate1").each(function() {
+            $(this).attr({
+                'data-aos': 'fade-up',
+                'data-aos-delay':'150'
+            });
+        });
+        $(".animate2").each(function() {
+            $(this).attr({
+                'data-aos': 'fade-up',
+                'data-aos-delay':'250'
+            });
+        });
+        $(".animate3").each(function() {
+            $(this).attr({
+                'data-aos': 'fade-up',
+                'data-aos-delay':'350'
+            });
+        });
+        $(".animate4").each(function() {
+            $(this).attr({
+                'data-aos': 'fade-up',
+                'data-aos-delay':'450'
+            });
+        });
+        $(".animate5").each(function() {
+            $(this).attr({
+                'data-aos': 'fade-up',
+                'data-aos-delay':'550'
+            });
+        });
+        AOS.refreshHard();
+    });
+    
+    $(window).load(function () {
+        AOS.refreshHard();
+    });
+	AOS.refreshHard();
+})(jQuery);
+
+
+
+}
+
+/*
+     FILE ARCHIVED ON 08:08:44 Mar 07, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:09:37 May 20, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.538
+  exclusion.robots: 0.061
+  exclusion.robots.policy: 0.05
+  esindex: 0.01
+  cdx.remote: 53.025
+  LoadShardBlock: 181.358 (3)
+  PetaboxLoader3.datanode: 79.055 (4)
+  PetaboxLoader3.resolve: 593.703 (2)
+  load_resource: 499.791
+*/
